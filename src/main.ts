3be52106cb042023130c95428efb29d46cc51b75
Clain Yann <yann.clain@gmail.com>
2022-03-31 10:27:06 +0200

    feat: add Tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
+import '@/assets/styles/main.scss'
+import '@/assets/styles/tailwind.scss'
 
 createApp(App).mount('#app')
